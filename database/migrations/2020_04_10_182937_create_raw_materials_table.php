19d98e0548714165eb98888a04c53e70f1ad1c6b
Heber <hu.quintero.mares@gmail.com>
2020-04-10 14:17:20 -0500

    ajustes de layout y migraciones

---
+++ database/migrations/2020_04_10_182937_create_raw_materials_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRawMaterialsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('raw_materials', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->biginteger('amount');
+            $table->float('price');
+            $table->date('expiration');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('raw_materials');
+    }
+}
