3daeb8806390940b9ec77ec3f2e71833f195ddec
LizF11 <af110502@gmail.com>
2020-05-08 17:03:58 -0500

    paginas a store y eliminacion de perfil de usuario default

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -62,14 +62,14 @@ scratch. This page gets rid of all links and provides the needed markup only.
             <div class="sidebar">
                 <!-- Sidebar user panel (optional) -->
                 <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-                    <div class="image">
+                    {{-- <div class="image">
                         <img src="{{asset('dist/img/user2-160x160.jpg')}}" class="img-circle elevation-2"
                             alt="User Image">
                     </div>
                     <div class="info">
                         <a href="#" class="d-block">Nombre de usuario</a>
                     </div>
-                </div>
+                </div> --}}
 
                 <!-- Sidebar Menu -->
                 <nav class="mt-2">
